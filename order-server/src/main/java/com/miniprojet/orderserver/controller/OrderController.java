391b4951ce77e7f4220cdefd6c0658a9521c5ad0
YiiSauce <youssefyouss321@gmail.com>
2023-12-25 19:42:41 +0100

    Adding the Order Service

---
+++ order-server/src/main/java/com/miniprojet/orderserver/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.miniprojet.orderserver.controller;
+
+import com.miniprojet.orderserver.dto.OrderRequest;
+import com.miniprojet.orderserver.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}
---
+++ order-server/src/main/java/com/miniprojet/orderserver/model/Order.java
@@ -0,0 +1,21 @@
+package com.miniprojet.orderserver.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "t_orders")
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+}
